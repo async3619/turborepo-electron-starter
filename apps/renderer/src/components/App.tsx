3bc54acb057e1b1bd3230b9ffed7284d3d2498db
Sophia <me@sophia-dev.io>
2023-08-16 00:33:33 +0900

    add material ui based renderer example codes

--- apps/renderer/src/components/App.tsx
+++ apps/renderer/src/components/App.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 
+import { ThemeProvider } from "@mui/material";
+
 import { ApolloProvider } from "@apollo/client";
 import apolloClient from "@graphql/client";
 
+import { theme } from "@styles/theme";
+
+import { Layout } from "@components/Layout";
+
 import { Routes } from "@pages";
 
-export interface AppProps {}
-
-export function App({}: AppProps) {
+export function App() {
     return (
         <ApolloProvider client={apolloClient}>
-            <Routes />
+            <ThemeProvider theme={theme}>
+                <Layout>
+                    <Routes />
+                </Layout>
+            </ThemeProvider>
         </ApolloProvider>
     );
 }
